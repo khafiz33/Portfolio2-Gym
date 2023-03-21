590dcbdcd52590d6f5d28f1452cc6ae3d5319c41
khafiz33 <khafiz.k33@gmail.com>
2023-03-21 18:12:16 +0500

    Trying to remove the scroll on the site's about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,13 +2,13 @@
 import './About.css';
 // import functions
 import React from 'react';
-// import components
-import Header from '../../components/Header/Header';
-// import images
-import HeaderImage from '../../assets/images/header_bg_1.jpg';
-import StoryImage from '../../assets/images/about1.jpg'
-import VisionImage from '../../assets/images/about2.jpg'
-import MissionImage from '../../assets/images/about3.jpg'
+// // import components
+// import Header from '../../components/Header/Header';
+// // import images
+// import HeaderImage from '../../assets/images/header_bg_1.jpg';
+// import StoryImage from '../../assets/images/about1.jpg'
+// import VisionImage from '../../assets/images/about2.jpg'
+// import MissionImage from '../../assets/images/about3.jpg'
 
 function About() {
   return (
